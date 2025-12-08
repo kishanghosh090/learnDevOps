439f4c2548f5368994bef266c67f750b54ef933a
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-08 10:58:26 +0530

    done docker compose with docker

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <>
+      <h1>welcome to chai code frontend</h1>
+      <h3>data {message}</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,5 @@
+➜ server docker build -t express-server .
+➜ server docker run -p 4000:4000 express-server
+
+➜  learnDevOps docker run -p 5174:3000 react-client:latest
+                            my port : docker port
