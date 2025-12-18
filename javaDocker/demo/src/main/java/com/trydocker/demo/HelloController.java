6c0be1e9d14eeefecf0629b2ae110e9d621ccd70
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-18 22:04:56 +0530

    learn about docker java

---
+++ javaDocker/demo/src/main/java/com/trydocker/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.trydocker.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String greet(){
+        return  "hello world";
+    }
+}
