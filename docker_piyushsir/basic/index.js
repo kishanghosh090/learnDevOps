830a2074db9fa60fc05be2caef0cbb6d50ba16f1
kishanghosh090 <kishanghosh090@gmail.com>
2026-03-07 22:00:22 +0530

    learn about host network map to container

--- docker_piyushsir/basic/index.js
+++ docker_piyushsir/basic/index.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT || 4411;
 
 const app = express();
 
-app.get((_req, res) => {
+app.get("/", (_req, res) => {
   return res.json({ message: "hello from chai code" });
 });
 
-app.listen(PORT, "192.168.101.2", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`server is listing at PORT ${PORT}`);
 });
---
+++ docker_piyushsir/basic/readme.md
@@ -0,0 +1,2 @@
+- restrict host can access(only)
+  docker run -p 192.168.101.2:3002:4411 appv1
