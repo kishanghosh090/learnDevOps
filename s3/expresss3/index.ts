46241dc841f11687c97f031043cbcada14601e35
kishanghosh090 <kishanghosh090@gmail.com>
2026-06-13 20:41:30 +0530

    add s3 configs

--- s3/expresss3/index.ts
+++ s3/expresss3/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import { createPresignedUrlWithClient } from "./s3";
 
 const app = express();
 
 const PORT = 5003;
-app.get("/presignedUrl", (req, res) => {
+app.get("/presignedUrl", async (req, res) => {
   // presigned URI
+  const url = await createPresignedUrlWithClient({
+    bucket: "ghoshkishanrana",
+    key: "file1" + `${Date.now()}` + ".png",
+  });
+  return res.status(200).json({ url });
 });
 app.listen(PORT, () => {
   console.log(`server is listing at PORT ${PORT}`);
--- s3/expresss3/s3.ts
+++ s3/expresss3/s3.ts
@@ -1,16 +1,14 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { client } from "./s3-client";
 
 export const createPresignedUrlWithClient = ({
-  region,
   bucket,
   key,
 }: {
-  region: string;
   bucket: string;
   key: string;
 }) => {
-  const client = new S3Client({ region });
   const command = new PutObjectCommand({ Bucket: bucket, Key: key });
   return getSignedUrl(client, command, { expiresIn: 3600 });
 };
