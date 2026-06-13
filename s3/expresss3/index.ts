c10b3c14e7679a926e2aebb4f2f795a5b9ac90be
kishanghosh090 <kishanghosh090@gmail.com>
2026-06-13 20:04:25 +0530

    init createPresignedUrlWithClient

--- s3/expresss3/index.ts
+++ s3/expresss3/index.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 
 const PORT = 5003;
-
+app.get("/presignedUrl", (req, res) => {
+  // presigned URI
+});
 app.listen(PORT, () => {
   console.log(`server is listing at PORT ${PORT}`);
 });
